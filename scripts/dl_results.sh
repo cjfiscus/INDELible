62b9bdcf897282930ce8a2e339ba1eff2309c568
cjfiscus <cfisc004@ucr.edu>
2017-12-07 14:12:13 -0800

    results download script

---
+++ scripts/dl_results.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+## Filtered vcf is hosted on figshare under a private link. Download with the following:
+
+wget -O ../results/filtered.vcf.gz https://ndownloader.figshare.com/files/9928819\?private_link\=77cb4ee59e590e5058d8
