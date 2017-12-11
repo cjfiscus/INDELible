65cdc69e8c8974f1e1298f14241fc87983a544e1
Glen Morrison <gmorr004@ucr.edu>
2017-12-11 10:34:36 -0800

    Commented off mkdir results, because this dir already exists and that created problems

--- scripts/dl_results.sh
+++ scripts/dl_results.sh
@@ -3,6 +3,6 @@
 ## Filtered vcf is hosted on figshare under a private link. Download with the following:
 
 cd ../ # cd into main project directory
-mkdir results # make and change into new results directory  
+#mkdir results # make and change into new results directory  
 
 wget -O ../results/filtered.vcf.gz https://ndownloader.figshare.com/files/9928819\?private_link\=77cb4ee59e590e5058d8
