9893535c5dd731ac110a5e9603a2e4b38b0188c7
Glen Morrison <gmorr004@ucr.edu>
2017-12-11 11:06:24 -0800

    Fixed issue with 4_indel_size_and_position.py: wrong vcf fields being selected, likely from earlier version of script

--- scripts/4_indel_size_and_position.py
+++ scripts/4_indel_size_and_position.py
@@ -9,22 +9,22 @@ input_file = sys.argv[1]
 output_file = sys.argv[2]
 
 # Make lists to store indel IDs, sizes and positions.. and also chromosome number.
-indel_ID = []
 position = []
-size = []
+ref = []
+alt = []
 chromosome = []
 # Loop through VCF file and store positions and sizes.
 import vcf
 vcf_reader = vcf.Reader(open(input_file, 'r'))
 for record in vcf_reader:
-	indel_ID.append(record.ID)
 	position.append(record.POS)
-	size.append(record.LEN)
+	ref.append(record.REF)
+	alt.append(record.ALT)
 	chromosome.append(record.CHROM)
 
 # Make lists into dataframe
 import pandas as pd
-dat = {"indel_ID" : indel_ID, "position" : position, "size" : size, "chromosome" : chomosome}
+dat = {"position" : position, "ref" : ref, "alt" : alt, "chromosome" : chomosome}
 dat_df = pd.DataFrame(dat)
 
 # Save data as .csv
