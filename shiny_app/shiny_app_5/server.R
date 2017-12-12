32b67b11135a39ebc7ffd7a0b09413f98555f6cb
Glen Morrison <gmorr004@ucr.edu>
2017-12-12 12:51:55 -0800

    Repaired error in server.R for shiny_app_5

--- shiny_app/shiny_app_5/server.R
+++ shiny_app/shiny_app_5/server.R
@@ -6,6 +6,6 @@ server <- function(input, output) {
 
 	# Generate a plot 
 	output$two_histsPlot <- renderPlot({
-	two_hists(x, chrm = input$chrm_select, density = input$dens_select)
+	two_hists(x = dat, chrm = input$chrm_select, density = input$dens_select)
 	})
 }
